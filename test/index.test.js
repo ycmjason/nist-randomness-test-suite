f2707acb9aa17b6a61d7075c0d38621c4f545cc1
Jason Yu <me@ycmjason.com>
2017-05-19 10:58:06 +0100

    added tests for allowed input

--- test/index.test.js
+++ test/index.test.js
@@ -3,41 +3,65 @@ process.env.NODE_ENV = 'test';
 var assert = require('assert');
 
 var TestSuit = require('../lib/index');
-var testNames = require('../lib/testNames');
+var testNames = TestSuit.testNames;
 
 var range = require('../lib/utils/range');
+
+const NUMBER_OF_BIT = Math.pow(10, 5);
 
 var perfect_generator = () => Math.round(Math.random());
 var zero_biased_generator = () => Math.random() > 0.65? 1: 0;
 var faulty_generator = () => 0;
+
+var perfect_bits = range(NUMBER_OF_BIT).map(perfect_generator);
+var zero_biased_bits = range(NUMBER_OF_BIT).map(zero_biased_generator);
+var faulty_bits = range(NUMBER_OF_BIT).map(faulty_generator);
 
 describe('NIST test suite', function(){
   // allow 10s to complete the test
   this.timeout(10000);
 
   testNames.forEach(name => {
-    describe(name + ' test', function(){
+    describe(name, function(){
       var skip;
       var testSuit = new TestSuit(0.001);
 
       skip = [];
       if(skip.indexOf(name) == -1){
         it('# perfect generator should pass', function(){
-          assert(testSuit[name + 'Test'](perfect_generator));
+          assert(testSuit[name](perfect_generator));
+        });
+        it('# perfect bits array should pass', function(){
+          assert(testSuit[name](perfect_bits));
+        });
+        it('# perfect bits should pass', function(){
+          assert(testSuit[name](perfect_bits.join('')));
         });
       }
 
-      skip = ['nonOverlappingTemplateMatching'];
+      skip = ['nonOverlappingTemplateMatchingTest'];
       if(skip.indexOf(name) == -1){
         it('# zero biased generator should fail', function(){
-          assert(!testSuit[name + 'Test'](zero_biased_generator));
+          assert(!testSuit[name](zero_biased_generator));
+        });
+        it('# zero biased bits array should fail', function(){
+          assert(!testSuit[name](zero_biased_bits));
+        });
+        it('# zero biased bits should fail', function(){
+          assert(!testSuit[name](zero_biased_bits.join('')));
         });
       }
 
-      skip = ['nonOverlappingTemplateMatching'];
+      skip = ['nonOverlappingTemplateMatchingTest'];
       if(skip.indexOf(name) == -1){
         it('# faulty generator should fail', function(){
-          assert(!testSuit[name + 'Test'](faulty_generator));
+          assert(!testSuit[name](faulty_generator));
+        });
+        it('# faulty bits array should fail', function(){
+          assert(!testSuit[name](faulty_bits));
+        });
+        it('# faulty bits should fail', function(){
+          assert(!testSuit[name](faulty_bits.join('')));
         });
       }
     });
